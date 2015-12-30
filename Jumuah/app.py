e31c2a5c49d1c4c1fe13739539183d7afa733df2
khalidAlnuaim <kaluaim@gmail.com>
2015-12-31 00:59:56 +0300

    adding main.py and makein it the entry point

--- Jumuah/app.py
+++ Jumuah/app.py
@@ -1,10 +1,4 @@
 from flask import Flask
-from flask import render_template
 
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-app.run(debug=True, port=8008)
+app.run(debug=True, port=8088)
