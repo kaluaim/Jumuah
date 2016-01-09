6ee66602ad3c60dbe53708bca36f5206478c733e
kaluaim <kaluaim@gmail.com>
2016-01-09 09:19:54 +0300

    adding views and templates and updating models

--- Jumuah/models.py
+++ Jumuah/models.py
@@ -2,17 +2,22 @@ from app import db
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    password = db.Column(db.String)
-    email = db.Column(db.String)
-    date_joined = db.Column(db.DateTime)
-    last_login = db.Column(db.DateTime)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    date_joined = db.Column(db.DateTime, nullable=False)
+    last_login = db.Column(db.DateTime, nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+    votes = db.relationship('Vote', backref='user', lazy='dynamic')
 
-    def __init__(self, name, password, email):
+    def __init__(self, name, password, email, date_joined, last_login, admin=False):
         self.name = name
         self.password = password
         self.email = email
+        self.date_joined = date_joined
+        self.last_login = last_login
+        self.admin = admin
 
     def __repr__(self):
         return '<User (name={}, password={}, email={})>'.format(self.name,
@@ -21,11 +26,12 @@ class User(db.Model):
 
 class Mosque(db.Model):
     __tablename__ = 'mosques'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
     location_id = db.Column(db.Integer, db.ForeignKey('locations.id'))
     imam_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    #date added
+    date_added = db.Column(db.DateTime, nullable=False)
+    topics = db.relationship('Topic', backref='mosque', lazy='dynamic')
 
     def __init__(self, name):
         self.name = name
@@ -36,11 +42,11 @@ class Mosque(db.Model):
 
 class Location(db.Model):
     __tablename__ = 'locations'
-    id = db.Column(db.Integer, primary_key=True)
-    country = db.Column(db.String)
-    province = db.Column(db.String)
-    city = db.Column(db.String)
-    district = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    country = db.Column(db.String, nullable=False)
+    province = db.Column(db.String, nullable=False)
+    city = db.Column(db.String, nullable=False)
+    district = db.Column(db.String, nullable=False)
 
     def __init__(self, country, province, city, district):
         self.country = country
@@ -56,13 +62,13 @@ class Location(db.Model):
 
 class Vote(db.Model):
     __tablename__ = 'votes'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     topic_id = db.Column(db.Integer, db.ForeignKey('topics.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    #date of vote
+    vote_date = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self):
-        pass
+    def __init__(self, vote_date):
+        self.vote_date = vote_date
 
     def __repr__(self):
         return '<Vote (topic={})>'.format(self.topic_id)
@@ -70,10 +76,11 @@ class Vote(db.Model):
 
 class Topic(db.Model):
     __tablename__ = 'topics'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
     mosque_id = db.Column(db.Integer, db.ForeignKey('mosques.id'))
     user_created_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    votes = db.relationship('Vote', backref='topic', lazy='dynamic')
 
     def __init__(self, title):
         self.title = title
--- Jumuah/views.py
+++ Jumuah/views.py
@@ -1,6 +1,30 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app import app
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('index'))
+
+
+@app.route('/mosque/')
+@app.route('/mosque/<int:mosque_id>')
+def mosque(mosque_id=-1):
+    if mosque_id is not -1:
+        return render_template('mosque.html', id=mosque_id)
+    else:
+        return render_template('mosques.html')
