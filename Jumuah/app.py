d1da609466f4869f47cbd14e260d1c5f955330fc
kaluaim <kaluaim@gmail.com>
2016-02-12 15:14:33 +0300

    adding google maps

--- Jumuah/app.py
+++ Jumuah/app.py
@@ -4,6 +4,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask.ext.bcrypt import Bcrypt
+from flask.ext.googlemaps import GoogleMaps
 import nexmo
 
 app = Flask(__name__)
@@ -17,5 +18,6 @@ login_manager.login_view = 'login'
 login_manager.login_message = u"فضلاً قم بتسجيل الدخول."
 login_manager.login_message_category = "info"
 bootstrap = Bootstrap(app)
+GoogleMaps(app)
 
 nexmo = nexmo.Client(key='742ebb74', secret='a273d2d8')
